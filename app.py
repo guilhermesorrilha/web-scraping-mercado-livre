9ff9b3bca8d54d6bb926859a50d7d853a46ee049
Guilherme <guisorrilha10@gmail.com>
2026-01-14 17:24:35 -0300

    Atualização para que o código pegue uma maior quantidade de produtos

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+import re
 
-item_escolha = input('Qual item do Mercado Livre você gostaria de consultar? ')
+item_escolha = "iphone 17" #input('Qual item do Mercado Livre você gostaria de consultar? ')
 item_escolha = item_escolha.strip().lower()
 item_escolha = item_escolha.replace(' ', '-')
 
-url = f'https://lista.mercadolivre.com.br/{item_escolha}'
 
+lista_podutos = []
+
+
+# PRIMEIRA PAGINA
+url = f'https://lista.mercadolivre.com.br/{item_escolha}'
 header = {
     'Connection': 'keep-alive',
     'device-memory': '8',
@@ -30,16 +36,52 @@ header = {
     'Sec-Fetch-Dest': 'document',
     'Referer': 'https://www.mercadolivre.com.br/',
 }
+response_primeiro = requests.get(url=url, headers=header)
+soup_primerio = BeautifulSoup(response_primeiro.text, 'html.parser')
+# =================================================================
 
-response = requests.get(url=url, headers=header)
-soup = BeautifulSoup(response.text, 'html.parser')
-produtos = soup.find_all('li', class_='ui-search-layout__item')
 
-for produto in produtos:
+# JOGA TODOS OS ITENS DA PAGINA NA LISTA (lista_podutos)
+for item in soup_primerio.find_all('li', class_='ui-search-layout__item'):
+    lista_podutos.append(item)
+# ==================================================================
+
+
+
+# ACHA O JSON DENTRO DO HTML E PEGA OS LINKS DAS PROXIMAS PAGINAS
+inicio = response_primeiro.text.find('>_n.ctx.r=')
+final = response_primeiro.text.find('"currentSnackbar":null,"isEshopsEnvironment":false}},"mainEntry":"___search-index"}')
+json = json.loads(response_primeiro.text[inicio + 10:final + 83])
+lista_paginas = json['appProps']['pageProps']['initialState']['pagination']['pagination_nodes_url']
+# ==================================================================
+
+
+
+# PERCORRE PAGINA POR PAGINA E PEGA SEUS ITENS E JOGA DENTRO DA LISTA
+for pagina in lista_paginas:
+    if pagina['value'] == "1":
+        continue
+
+    print(pagina)
+
+    url = pagina['url']
+    response_demais_paginas = requests.get(url=url, headers=header)
+    soup_demais_paginas = BeautifulSoup(response_demais_paginas.text, 'html.parser')
+
+    for item in soup_demais_paginas.find_all('li', class_='ui-search-layout__item'):
+        lista_podutos.append(item)
+# ==================================================================
+
+
+
+
+for posicao, produto in enumerate(lista_podutos):
     titulo = produto.find('h3', class_='poly-component__title-wrapper').text
     preco = produto.find('span', class_='andes-money-amount andes-money-amount--cents-superscript').text
     img = produto.find('img', class_='poly-component__picture')
     img_url = img.get('src')
     print(f'{titulo}\n{preco}\n{img_url}\n{'-'*75}')
+    if posicao == 99:
+        break
 
 
